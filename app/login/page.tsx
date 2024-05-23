bc8dde0ebc83c8f3fc418a0aa835655cf3044525
miguelbidarrab <miguelbidarrab.com>
2024-05-23 15:47:14 +0100

    Initial Commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@ import { headers } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import { SubmitButton } from "./submit-button";
+import { login, signup } from "./actions";
 
 export default function Login({
   searchParams,
@@ -74,6 +75,8 @@ export default function Login({
         Back
       </Link>
 
+      <h1 className="text-3xl font-bold">Supabase Next.js Starter down here</h1>
+
       <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
         <label className="text-md" htmlFor="email">
           Email
@@ -113,6 +116,38 @@ export default function Login({
             {searchParams.message}
           </p>
         )}
+      </form>
+
+      <h1 className="text-3xl font-bold">User Managment App down here</h1>
+      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+        <label htmlFor="email">Email:</label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          id="email"
+          name="email"
+          type="email"
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          id="password"
+          name="password"
+          type="password"
+          required
+        />
+        <button
+          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+          formAction={login}
+        >
+          Log in
+        </button>
+        <button
+          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+          formAction={signup}
+        >
+          Sign up
+        </button>
       </form>
     </div>
   );
